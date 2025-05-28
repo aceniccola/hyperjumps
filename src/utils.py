ceaf53dde2a0eb659588787de54777b95ce0a4e3
Andrew Ceniccola <109996166+aceniccola@users.noreply.github.com>
2025-05-28 01:15:25 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@
 def reversedFull(forwardList):
     return [[[deepElem[-i] for i in range(1,len(deepElem)+1)] for deepElem in elem] for elem in forwardList]
 
-# copies a list with on element `lost` (removed)
+# copies a list with one element `lost` (removed)
 def copCre(copied, lost):
     copy = copied.copy()
     copy.remove(lost)
